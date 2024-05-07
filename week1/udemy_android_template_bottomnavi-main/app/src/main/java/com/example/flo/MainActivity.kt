08de18fa5fb44b4bc9b5a689c2d3fe5c0a72a8de
Lee Sung Jae <sa478@naver.com>
2024-05-07 20:42:27 +0900

    4주차 내용

--- week1/udemy_android_template_bottomnavi-main/app/src/main/java/com/example/flo/MainActivity.kt
+++ week1/udemy_android_template_bottomnavi-main/app/src/main/java/com/example/flo/MainActivity.kt
@@ -13,21 +13,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_FLO) // splash 테마에서 원래 테마로 전환 (onCreate가 실행 됐다는 건 준비 됐다는 이야기
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val song = Song(binding.mainMiniplayerTitleTv.text.toString(), binding.mainMiniplayerSingerTv.text.toString()) //  두 값을 사용하여 Song 클래스의 새 인스턴스를 생성
+        initBottomNavigation()
+
+        val song = Song(binding.mainMiniplayerTitleTv.text.toString(), binding.mainMiniplayerSingerTv.text.toString(),0 ,60, false) //  두 값을 사용하여 Song 클래스의 새 인스턴스를 생성
 
         binding.mainPlayerCl.setOnClickListener{
             // startActivity(Intent(this, SongActivity::class.java))
             val intent = Intent(this, SongActivity::class.java)
             intent.putExtra("title", song.title) // 제목을 title이라는 키 값으로 담는다
             intent.putExtra("singer", song.singer) // 가수명을 singer이라는 키 값으로 담는다
+            intent.putExtra("second", song.second)
+            intent.putExtra("playTime", song.playTime)
+            intent.putExtra("isPlaying", song.isPlaying)
+
             startActivity(intent) // 이 3줄은 주석처리 한 첫번째 줄과 같다. 이를 통해 택배를 보내고 SongActivity에서 받는다
         }
-        initBottomNavigation()
-
-
     }
 
     private fun initBottomNavigation(){
--- week1/udemy_android_template_bottomnavi-main/app/src/main/java/com/example/flo/SongActivity.kt
+++ week1/udemy_android_template_bottomnavi-main/app/src/main/java/com/example/flo/SongActivity.kt
@@ -2,6 +2,7 @@ package com.example.flo
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.flo.databinding.ActivitySongBinding
@@ -9,36 +10,109 @@ import com.example.flo.databinding.ActivitySongBinding
 class SongActivity :AppCompatActivity() {
 
     lateinit var binding : ActivitySongBinding //lateinit은 선언은 지금 하고 초기화는 나중에 한다
-
+    lateinit var song : Song
+    lateinit var timer : Timer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySongBinding.inflate(layoutInflater) // 설정
         setContentView(binding.root) // activity_song.xml의 맨 위로 이동
-        
+
+        initSong() // 데이터 받아오기
+        setPlayer(song)
+
         binding.songSettingBack.setOnClickListener { // Back 버튼 누르면 Fragment 종료
             finish()
         }
         binding.songPlayIv.setOnClickListener{ // 재생버튼 누르면 일시정지 버튼 나오기
-            setPlayerStatus(false)
+            setPlayerStatus(true)
         }
 
         binding.songPauseIv.setOnClickListener{ // 일시정지 누르면 재생 버튼 나오기
-            setPlayerStatus(true)
+            setPlayerStatus(false)
         }
-        if(intent.hasExtra("title") && intent.hasExtra("singer")){// 물건이 올 수도 있고 안 올 수도 있으니 if문 사용
-            binding.songTitleTv.text = intent.getStringExtra("title") // intent 택배 상자에서 title 키값을 가진 text로 바꾼다
-            binding.songTitleSingerTv.text = intent.getStringExtra("singer")
-        }
+
     }
-    
-    fun setPlayerStatus(isPlaying : Boolean){ // songPlayIv 버튼 클릭시 실행되는 함수
-        if(isPlaying){
+
+    override fun onDestroy() { // 앱이 꺼질 때 실행되는 함수. try-catch문 테스트 하려고 씀
+        super.onDestroy()
+        timer.interrupt()
+    }
+
+    private fun initSong(){ // 4주차 내용
+        if(intent.hasExtra("title") && intent.hasExtra("singer")){
+            song = Song(
+                intent.getStringExtra("title")!!,
+                intent.getStringExtra("singer")!!,
+                intent.getIntExtra("second", 0),
+                intent.getIntExtra("playTime",0),
+                intent.getBooleanExtra("isPlaying", false)
+            )
+        }
+        startTimer()
+    }
+
+    private fun setPlayer(song : Song){ // 정보 받아오기
+        binding.songTitleTv.text = intent.getStringExtra("title")!! // intent 택배 상자에서 title 키값을 가진 text로 바꾼다
+        binding.songTitleSingerTv.text = intent.getStringExtra("singer")!!
+        binding.songStartTimeTv.text = String.format("%02d:%02d", song.second / 60, song.second%60)
+        binding.songEndTimeTv.text = String.format("%02d:%02d", song.playTime / 60, song.playTime%60)
+        binding.songProgressbarSb.progress = (song.second * 1000 / song.playTime)
+
+        setPlayerStatus(song.isPlaying)
+    }
+
+    private fun setPlayerStatus(isPlaying : Boolean){ // songPlayIv 버튼 클릭시 실행되는 함수
+        song.isPlaying = isPlaying
+        timer.isPlaying = isPlaying
+
+        if(isPlaying){ // 실행중이면
+            binding.songPlayIv.visibility = View.GONE // 재생 버튼 숨기기
+            binding.songPauseIv.visibility = View.VISIBLE // 일시정지 버튼 보이기
+        }
+        else{ // 일시정지 되면
             binding.songPlayIv.visibility = View.VISIBLE // 재생 버튼 보이기
             binding.songPauseIv.visibility = View.GONE // 일시정지 버튼 숨기기
         }
-        else{
-            binding.songPlayIv.visibility = View.GONE // 재생 버튼 숨기기
-            binding.songPauseIv.visibility = View.VISIBLE // 일시정지 버튼 보이기
+    }
+
+    private fun startTimer(){
+        timer = Timer(song.playTime, song.isPlaying)
+        timer.start()
+    }
+
+    inner class Timer(private val playTime: Int, var isPlaying: Boolean = true):Thread(){
+        private var second : Int = 0
+        private var mills : Float = 0f
+
+        override fun run() {
+            super.run()
+            try { // 문제 없으면 실행
+                while (true){ // 타이머는 계속 반복
+
+                    if(second >= playTime){
+                        break
+                    }
+
+                    if(isPlaying){
+                        sleep(50)
+                        mills += 50
+
+                        runOnUiThread{ // 프로그레스 바를 건들려면 핸들러 or runOnUiThread 사용
+                            binding.songProgressbarSb.progress = ((mills / playTime)*100).toInt()
+                        }
+
+                        if(mills % 1000 ==0f){ // mills가 1000 될 때마다 second 증가
+                            runOnUiThread{
+                                binding.songStartTimeTv.text = String.format("%02d:%02d", second / 60, second%60)
+                            }
+                            second++
+                        }
+                    }
+                }
+            }catch (e: InterruptedException){ // 예외 처리
+                Log.d("Song", "쓰레드가 죽었습니다. ${e.message}")
+            }
+
         }
     }
 }
